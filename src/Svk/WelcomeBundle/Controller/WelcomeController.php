75ef0f0f4927f41c064226c511081b200937909d
JulesMarcil <jules.marcilhacy@gmail.com>
2013-11-02 19:19:25 +0000

    update form

--- src/Svk/WelcomeBundle/Controller/WelcomeController.php
+++ src/Svk/WelcomeBundle/Controller/WelcomeController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request,
     Symfony\Component\HttpFoundation\JsonResponse;
 
+use Svk\WelcomeBundle\Entity\Subscriber;
+
 class WelcomeController extends Controller
 {
     public function indexAction()
@@ -17,10 +19,30 @@ class WelcomeController extends Controller
 
     public function subscribeAction(Request $request)
     {
-    	$data = $request->request->all();
+    	// just setup a fresh $task object (remove the dummy data)
+	    $subscriber = new Subscriber();
 
-    	return new JsonResponse($data);
+	    $form = $this->createFormBuilder($subscriber)
+	        ->add('firstname', 'text')
+	        ->add('lastname', 'text')
+	        ->add('email', 'email')
+	        ->add('birthday', 'date')
+	        ->add('Subscribe', 'submit')
+	        ->getForm();
 
-    	return $this->redirect($this->generateUrl('svk_welcome_homepage'));
+	    $form->handleRequest($request);
+
+	    if ($form->isValid()) {
+	        // fait quelque chose comme sauvegarder la tâche dans la bdd
+	        $em = $this->getDoctrine()->getManager();
+		    $em->persist($subscriber);
+		    $em->flush();
+		    
+	        return $this->redirect($this->generateUrl('svk_welcome_homepage'));
+	    }
+
+    	return $this->render('SvkWelcomeBundle::subscriptionForm.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
